51e453f8f5fdd3eb786892652355781fa0203149
pratik <pratik.shekhar.khaire@gmail.com>
2025-01-23 16:05:09 +0530

    Adding the right and bottom card

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,7 @@
+import AttendanceChat from "@/components/AttendanceChat";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -17,16 +20,22 @@ const AdminPage = () => {
         <div className=" flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART  */}
           <div className="w-full lg:w-1/3 h-[450px] ">
-          <CountChart/>
+            <CountChart />
           </div>
           {/* ATTENDANCE CHART  */}
-          <div className="w-full lg:w-2/3 h-[450px] "></div>
+          <div className="w-full lg:w-2/3 h-[450px] ">
+            <AttendanceChat />
+          </div>
         </div>
         {/* BOTTOM CHART */}
-        <div className="">hi</div>
+        <div className=" w-full h-[500px]">
+          <FinanceChart/>
+        </div>
       </div>
       {/* RIGHT  */}
-      <div className="w-full lg:w-1/3">right</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      <EventCalendar/>
+      </div>
     </div>
   );
 };
