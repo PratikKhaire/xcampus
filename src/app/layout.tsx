a9ad6fdf2d8224ac1e1c9d2e50dbf0bf8e4d287a
pratik <pratik.shekhar.khaire@gmail.com>
2025-01-23 13:47:17 +0530

    Create the admin view with count component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "X-Campus",
+  
   description: "Next.js School Management System",
 };
 
